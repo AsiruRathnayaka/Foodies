0ebe929bd12faa7808e9fcdff38ef563c2b7b6e6
AsiruRathnayaka <it20602550@my.sliit.lk>
2023-04-05 16:19:24 +0530

    Initial Commit

---
+++ src/Screens/Feed/Components/Header.js
@@ -0,0 +1,20 @@
+import { Grid } from "@mui/material";
+import React from "react";
+
+const Header = () => {
+  return (
+    <Grid container direction="row" spacing={2}>
+      <Grid item xs={2}>
+            abc
+      </Grid>
+      <Grid item xs={6}>
+            abc
+      </Grid>
+      <Grid item xs={4}>
+            abc
+      </Grid>
+    </Grid>
+  );
+};
+
+export default Header;
---
+++ src/Screens/Feed/FeedScreen.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const FeedScreen = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default FeedScreen
